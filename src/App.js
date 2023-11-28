bf2be8227dda20f4b8d859081e3e0ac6a08b62f3
Ashish00Singh <ashish7232singh@gmail.com>
2023-11-28 12:49:33 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import Countdown from './Component/Countdown';
+import { useState } from 'react';
 
 function App() {
+  const [Seconds, setseconds] = useState(0)
+    if(Seconds<=59){
+        setTimeout(()=>{
+            setseconds(Seconds+1)
+        },100);
+    }if(Seconds==60){
+      clearInterval(id);
+    }
+    
+    const [Min, setMin] = useState(0)
+    if(Min<=59){
+        setTimeout(()=>{
+            setMin(Min+1)
+        },61000);
+    }
+    
   return (
     <div>
       
-      <Countdown></Countdown>
+      <Countdown Seconds={Seconds} Menutes={Min}></Countdown>
     
     </div>
   );
 }
-const myname = <bar type="opop" />
 
 export default App;
--- src/Component/Countdown.js
+++ src/Component/Countdown.js
@@ -1,7 +1,7 @@
-import React from 'react'
 import props from 'react'
 
-export default function Countdown() {
+export default function Countdown(props) {
+    
   return (
     <>
     <center>
@@ -12,24 +12,25 @@ export default function Countdown() {
     <div className='col-sm-4' >
         <div style={{backgroundColor:'blue', height: '250px', width: '200px'}}>
         <h1>Hour</h1>
-        <p> {props.type}</p>
+        <label value={0}></label>
         </div>
     </div>
     <div className='col-sm-4' >
         <div style={{backgroundColor:'blue', height: '250px', width: '200px'}}>
         <h1>Minuter</h1>
+        <p>{props.Menutes}</p>
         </div>
     </div>
     <div className='col-sm-4' >
         <div style={{backgroundColor:'blue', height: '250px', width: '200px'}}>
-        <h1>Second</h1>
+            <h1>Sceconds</h1>
+        <p id='op'>{props.Seconds}</p>
         </div>
     </div>
 
     </div></center>
     
     </>
-  )
-  const as =type ="ashsihs";
+  );
 }
  
